29d3173181f848c3c49063acf85a3d721ced163b
Zantana Kahsay <91621786+KahsayZantana@users.noreply.github.com>
2022-03-09 05:01:12 +0000

    ..

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World, from Express!');
-});
+  res.send('Hello World from Express!')
+})
 
 app.get('/about', (req, res) => {
-  res.send('About page, from Express!');
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  res.send('Hello World from Express, by Frank!')
+})
